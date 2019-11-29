cacfbe2639afd184c525f5e3e9c238e39cf725b6
Anton <aachizhov@sberbank.ru>
2019-11-29 22:28:49 +0500

    Добавлено задание 3 из практики 4

---
+++ Practice_4/Task3/Task3.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Integer[] integers = {11,3,2,2,5,6,1,2,7,8,8,9,9,9,9};
+        ArrayList<Integer> intList = new ArrayList();
+        for (Integer integer: integers){
+            intList.add(integer);
+        }
+
+        Collection<Integer> coll = removeDuplicates(intList);
+        System.out.println(coll);
+    }
+
+    public static Collection<Integer> removeDuplicates(Collection<Integer> collection){
+        return new HashSet<Integer>(collection);
+    }
+}
